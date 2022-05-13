4420bee191a447db602074f331eab59c9e939203
vuong.tran <vuong.tran@tiki.vn>
2022-05-13 20:37:55 +0700

    Delete lib folder

--- lib/package/converter.d.ts
+++
@@ -1,5 +0,0 @@
-export default LunarSolarConverter;
-declare class LunarSolarConverter {
-    convertSolar2Lunar(dd: any, mm: any, yy: any, timeZone: any): any[];
-    convertLunar2Solar(lunarDay: any, lunarMonth: any, lunarYear: any, lunarLeap: any, timeZone: any): number[];
-}
